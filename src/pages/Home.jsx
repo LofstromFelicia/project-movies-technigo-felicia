fff1e14e49274789a90e7020d6e804a88731c1be
Felicia Löfström <Flizan_Linnea@hotmail.com>
2026-06-15 11:30:45 +0200

    add: Home.jsx & MovieDetail.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Populära Filmer</h1>
+      <p>Här kommer filmnyheterna att visas snart...</p>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/MovieDetail.jsx
@@ -0,0 +1,10 @@
+const MovieDetail = () => {
+  return (
+    <div className="detail-container">
+      <h1>Filmrubrik</h1>
+      <p>Här kommer vi att visa bio-detaljer, betyg och beskrivning...</p>
+    </div>
+  )
+}
+
+export default MovieDetail
